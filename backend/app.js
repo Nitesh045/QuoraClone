e38aeb0981311b70999f12edd313045c6d957c7b
Nitesh Yadav <niteshyadav604@gmail.com>
2024-02-13 20:21:31 +0530

    change file

--- backend/app.js
+++ backend/app.js
@@ -28,14 +28,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 // path
 const path = require('path')
-app.use('/uploads',express.static(path.join(__dirname,"/../uploads")));
-app.use(express.static(path.join(__dirname,"/../Quora/dist")));
-//app.use(express.static(path.join(__dirname, '/Quora/dist')));
+// app.use('/uploads',express.static(path.join(__dirname,"./")));
+//app.use(express.static(path.join(__dirname,"../Quora/dist")));
+app.use(express.static(path.join(process.cwd(),"Quora",'dist',)));
 
 app.get("/",(req,res)=>{
     try {
 
-        res.sendFile(path.join(process.cwd(),"../Quora/dist/index.html"))
+        res.sendFile(path.join(process.cwd(),"Quora",'dist','index.html'))
     } catch (error) {
         console.log(error);
     }
